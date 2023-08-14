2f7e123a38de4c6b83e2177cfd53b763da9812af
hustle2live <kuz.v@ukr.net>
2023-08-14 10:51:43 +0300

    add change sliders position script

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// import './styles.module.scss';
-// import './styles.css';
+const gallerySlidesNodeElement = document.querySelector('.our-clients__gallery');
+const gallerySlidesNodeList = document.querySelectorAll('.card');
+
+const sliderPositionStart = 0;
+const sliderPositionEnd = gallerySlidesNodeList.length - 1;
+
+let sliderCount = sliderPositionStart;
+
+const setPositionGallery = () => (gallerySlidesNodeElement.style.left = '-' + sliderCount + '000px');
+
+const changeSliderCounter = (num) =>
+   (sliderCount = num > sliderPositionEnd ? sliderPositionStart : num < sliderPositionStart ? sliderPositionEnd : num);
+
+const prevSlide = () => setPositionGallery(changeSliderCounter(--sliderCount));
+const nextSlide = () => setPositionGallery(changeSliderCounter(++sliderCount));
+
+document.querySelector('.prev-btn').addEventListener('click', prevSlide);
+document.querySelector('.next-btn').addEventListener('click', nextSlide);
