98cffff670e4b8d89503354a30f705042b9e2fc3
hustle2live <kuz.v@ukr.net>
2023-09-21 11:16:43 +0300

    added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+
+// Load plugins
+const cssnano = require('gulp-cssnano');
+const changed = require('gulp-changed');
+const browsersync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const clean = require('gulp-clean');
+
+// clean build
+
+function clear() {
+   return src('./bulid/*', {
+      read: false
+   }).pipe(clean());
+}
+
+// CSS
+
+function css() {
+   const source = './src/css/styles.css';
+
+   return src(source).pipe(changed(source)).pipe(cssnano()).pipe(dest('./build/css/')).pipe(browsersync.stream());
+}
+
+// Optimize images
+
+function img() {
+   return src('./src/images/*').pipe(imagemin()).pipe(dest('./build/images'));
+}
+
+// html
+
+function html() {
+   return src('./src/*.html').pipe(dest('./build')).pipe(browsersync.stream());
+}
+
+// watch files
+
+function watchFiles() {
+   watch('./src/css/*', css);
+   watch('./src/*html', html);
+   watch('./src/images/*', img);
+}
+
+// browsersync
+
+function browserSync() {
+   browsersync.init({
+      server: {
+         baseDir: './build'
+      },
+      port: 3000
+   });
+}
+
+exports.watch = parallel(watchFiles, browserSync);
+exports.defauls = series(clear, parallel(html, css, img));
