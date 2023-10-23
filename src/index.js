f1025427a00cc232263e9cb23dfef5e5496c3e9d
hustle2live <kuz.v@ukr.net>
2023-10-23 21:28:52 +0300

    feedback wrapper added html and styles

--- src/index.js
+++ src/index.js
@@ -1,19 +1,59 @@
 const galleryElement = document.querySelector('.our-clients__gallery');
 const slidersNodeList = document.querySelectorAll('.card');
+const wrapperFeedback = document.querySelector('.feedback-wrapper');
+const buttonFeedbackClose = document.querySelector('.feedback-wrapper .btn-close');
+const formFeedback = document.querySelector('.feedback-wrapper form');
+const ctaButtonsAll = document.querySelectorAll('.cta-button');
+
+const showHideFeedbackWrapper = () => wrapperFeedback.classList.toggle('hidden');
+
+ctaButtonsAll.forEach((btn) => btn.addEventListener('click', showHideFeedbackWrapper));
+buttonFeedbackClose.addEventListener('click', showHideFeedbackWrapper);
+
+// POST API TO TELEGRAM BOT
+
+const postFeedback = async (TOKEN, METHOD_NAME = 'sendMessage', PARAMS) => {
+   const response = await fetch(`https://api.telegram.org/bot${TOKEN}/${METHOD_NAME}?${PARAMS}`, {
+      method: 'POST'
+   });
+   if (!response.ok) throw new Error("User's registration Failed. Error");
+   const data = await response.json();
+   console.log(data);
+   return data;
+};
+
+const tg_user = '843486240'; // id пользователя, для отправки сообщения
+const bot_token = '6717439509:AAHaPvuHO3WORt6_3p3CMIJgoCp5fECbX8s'; // токен бота
+const chat_id = '';
+const textMessage = 'Первая строка сообщения со ссылкой \n Вторая строка с жирным текстом';
+// const params = '';
+
+// параметры, которые отправятся в api телеграм
+const params = {
+   chat_id: tg_user, // id получателя
+   text: textMessage, // текст сообщения
+   parse_mode: 'HTML'
+}; // режим отображения сообщения HTML (не все HTML теги работают)
+
+const paramsQuery = (id, text) => {
+   if (!id || !text) return;
+
+   return `chat_id=${id}&text=${text}`;
+};
+
+console.log(JSON.stringify(params));
+
+// <--/ POST API TO TELEGRAM BOT -->
 
 const galleryFullWidth = () => galleryElement.offsetWidth;
 const sliderWidth = () => slidersNodeList[0].offsetWidth;
 
-
-// console.log(galleryFullWidth());
-// console.log(sliderWidth());
-
-const sliderPosEnd = () => galleryFullWidth() - sliderWidth();
 const sliderPosStart = 0;
+const sliderPosEnd = () => galleryFullWidth() - sliderWidth();
+
 let sliderPosition = 0;
 
 const checkingIsSliderPoscorrectly = () => sliderPosition % sliderWidth();
-
 
 const calcSliderPos = (next = false) => {
    const tempPosition = next ? sliderPosition + sliderWidth() : sliderPosition - sliderWidth();
@@ -29,10 +69,6 @@ const calcSliderPos = (next = false) => {
 };
 
 const setPosGallery = () => {
-   // console.log('sliderWidth():' + sliderWidth());
-   // console.log('sliderPosition:' + sliderPosition);
-   // console.log(sliderPosition % sliderWidth());
-
    galleryElement.style.left = '-' + sliderPosition + 'px';
    galleryElement.style.transform = `translateX(-)${sliderPosition}px`;
 };
@@ -42,5 +78,3 @@ const nextSlide = () => setPosGallery(calcSliderPos(true));
 
 document.querySelector('.prev-btn').addEventListener('click', prevSlide);
 document.querySelector('.next-btn').addEventListener('click', nextSlide);
-
-// console.log(checkingIsSliderPoscorrectly());
