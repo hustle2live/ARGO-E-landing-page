3db31798b9bf7930bfe65978367abf2126bf82fb
hustle2live <kuz.v@ukr.net>
2023-11-27 23:47:24 +0200

    add animation section about

--- src/observer.js
+++ src/observer.js
@@ -1,8 +1,16 @@
 // observer.js
-function onEntry(entry) {
-   entry.forEach((change) => {
-      if (change.isIntersecting) {
-         change.target.classList.add('customAnimationIn');
+function onEntry(nodeList) {
+   nodeList.forEach((domElem) => {
+      if (domElem.isIntersecting) {
+         domElem.target.classList.add('customAnimationIn');
+      }
+   });
+}
+
+function onScale(nodeList) {
+   nodeList.forEach((domElem) => {
+      if (domElem.isIntersecting) {
+         domElem.target.classList.add('customAnimationScale');
       }
    });
 }
@@ -11,12 +19,20 @@ const options = {
    threshold: [0.5]
 };
 
-const observer = new IntersectionObserver(onEntry, options);
+const moveObserver = new IntersectionObserver(onEntry, options);
 
-const elements = document.querySelectorAll('.animated');
+const scaleObserver = new IntersectionObserver(onScale, options);
 
-for (const elem of elements) {
-   observer.observe(elem);
+const elementsCard = document.querySelectorAll('.services__card.animated');
+const elementsIcon = document.querySelectorAll('.icon.animated');
+
+for (const elem of elementsCard) {
+   moveObserver.observe(elem);
+}
+
+for (const elem of elementsIcon) {
+   scaleObserver.observe(elem);
 }
 
 // console.log(elements);
+console.log(elementsIcon);
