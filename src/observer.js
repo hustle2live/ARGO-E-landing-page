407c5014148086efefaeb42e5547a584e0781ccd
hustle2live <kuz.v@ukr.net>
2023-11-29 18:57:33 +0200

    extra-services animations add

--- src/observer.js
+++ src/observer.js
@@ -2,7 +2,7 @@
 
 const animationIn = 'customAnimationIn';
 const animationScale = 'customAnimationScale';
-// const animationSlideInLeft = 'slideInLeft';
+const animationSlideInLeft = 'slideInLeft';
 const animationSlideInLeftRotate = 'slideInLeftRotate';
 const animationSlideInRight = 'slideInRight';
 
@@ -12,6 +12,11 @@ const iconsAboutAll = document.querySelectorAll('.icon.animated');
 
 const serviceDescriptionPicture = document.querySelector('.service-description__image.animated');
 const serviceDescriptionText = document.querySelector('.service-description__service-types.animated');
+
+const extraServicesContainer = document.querySelector('.extra-services__content.container');
+const animatedCardsRightAll = document.querySelectorAll('.animated.right');
+const animatedCardsLeftAll = document.querySelectorAll('.animated.left');
+const animatedCardsCenterAll = document.querySelectorAll('.animated.center');
 
 const defaultOptions = {
    threshold: [0.5]
@@ -43,9 +48,39 @@ const serviceSectionEntryCallback = (entries) => {
    });
 };
 
+const extraServiceSectionEntryCallback = (entries) => {
+   entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+         for (const card of animatedCardsRightAll) {
+            card.classList.add(animationSlideInRight);
+         }
+         for (const card of animatedCardsLeftAll) {
+            card.classList.add(animationSlideInLeft);
+         }
+         for (const card of animatedCardsCenterAll) {
+            card.classList.add(animationScale);
+         }
+      } else {
+         for (const card of animatedCardsRightAll) {
+            card.classList.remove(animationSlideInRight);
+         }
+         for (const card of animatedCardsLeftAll) {
+            card.classList.remove(animationSlideInLeft);
+         }
+         for (const card of animatedCardsCenterAll) {
+            card.classList.remove(animationScale);
+         }
+      }
+   });
+};
+
 observer(serviceSectionEntryCallback, {
    threshold: [0.25]
 }).observe(servicesContainer);
+
+observer(extraServiceSectionEntryCallback, {
+   threshold: [0.25]
+}).observe(extraServicesContainer);
 
 for (const elem of iconsAboutAll) {
    observer((e) => onEntryCallback(e, animationScale)).observe(elem);
