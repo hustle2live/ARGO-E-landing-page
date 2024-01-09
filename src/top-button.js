a2a24139700b19da332b7f1b05ff549a2bf134bd
hustle2live <kuz.v@ukr.net>
2024-01-09 18:20:42 +0200

    add nav-menu button

--- src/top-button.js
+++ src/top-button.js
@@ -1,4 +1,9 @@
-let topButton = document.querySelector('.top-btn');
+const topButton = document.querySelector('.top-btn');
+const menuButton = document.querySelector('.menu-btn');
+const openClosedNodes = document.querySelectorAll('.open.closed');
+
+const navMenuOpen = document.querySelector('.nav-menu.open');
+const navMenuList = document.querySelector('.nav-menu__list');
 
 function scrollFunction() {
    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -11,3 +16,19 @@ function scrollFunction() {
 window.onscroll = function () {
    scrollFunction();
 };
+
+function menuOpenClose() {
+   openClosedNodes.forEach((element) => element.classList.toggle('closed'));
+}
+
+const isMenuOpened = () => !navMenuOpen.classList.contains('closed');
+
+menuButton.addEventListener('click', menuOpenClose);
+
+navMenuList.addEventListener('click', (e) => {
+   // console.log(e.target);
+   if (isMenuOpened()) {
+      menuOpenClose();
+   }
+   return;
+});
